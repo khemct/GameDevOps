c282706c4681f9e3900cac7832fa66c6573ea8f6
KhemCT <chadchapong11043@gmail.com>
2025-03-05 21:27:36 +0700

    (Added rectangle animation and FPS counter)
    GamePanel.java:
    - Added automatic rectangle movement with direction change upon hitting window edges.
    - Implemented color randomization for the rectangle on movement.
    - Added FPS counter to measure rendering performance.
    - Modified paintComponent() to update and repaint the rectangle continuously.
    
    GameWindow.java:
    - Adjusted window size to 400x400.
    - Ensured the window is centered on the screen using setLocationRelativeTo(null).

--- MyGameCore/src/main/java/game/GameWindow.java
+++ MyGameCore/src/main/java/game/GameWindow.java
@@ -7,7 +7,7 @@ public class GameWindow {
 
     public GameWindow(GamePanel gamePanel){
         jFrame = new JFrame();
-        jFrame.setSize(500,500);
+        jFrame.setSize(400,400);
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jFrame.add(gamePanel);
         jFrame.setLocationRelativeTo(null);
