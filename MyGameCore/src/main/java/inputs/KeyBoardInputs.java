363176aced043335d7a208cdfd03b5bc2f22bf09
KhemCT <chadchapong11043@gmail.com>
2025-03-12 19:10:27 +0700

    (Implemented player jump and gravity mechanics)
    Player.java:
    - Added jump and gravity mechanics to handle player’s vertical movement.
    - Introduced `airSpeed` for controlling jump force and falling speed.
    - Implemented `gravity` to apply constant downward force when the player is in the air.
    - Created `jump()` method to allow the player to jump, adjusting `airSpeed` for smooth vertical movement.
    - Implemented `resetInAir()` to reset the jumping state when the player lands on the floor.
    - Added `updatePos()` to manage both horizontal and vertical movement with collision checks.
    - Integrated `inAir` state to track when the player is airborne and adjust gravity/airSpeed accordingly.
    
    HelpMethods.java:
    - Created `IsEntityOnFloor()` to check if the player is touching the ground or is in the air.
    - Updated `CanMoveHere()` to ensure the player can move based on the current position, checking for collisions in both horizontal and vertical directions.

--- MyGameCore/src/main/java/inputs/KeyBoardInputs.java
+++ MyGameCore/src/main/java/inputs/KeyBoardInputs.java
@@ -33,6 +33,9 @@ public class KeyBoardInputs implements KeyListener {
             case KeyEvent.VK_D:
                 gamePanel.getGame().getPlayer().setRight(true);
                 break;
+            case  KeyEvent.VK_SPACE:
+                gamePanel.getGame().getPlayer().setJump(true);
+                break;
         }
 
     }
@@ -51,6 +54,9 @@ public class KeyBoardInputs implements KeyListener {
                 break;
             case KeyEvent.VK_D:
                 gamePanel.getGame().getPlayer().setRight(false);
+                break;
+            case  KeyEvent.VK_SPACE:
+                gamePanel.getGame().getPlayer().setJump(false);
                 break;
         }
 
