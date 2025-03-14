2116e07dc5f0bfdcb9ddab84f2e6c688f2f2d75b
Nick <nicky.ch2004@gmail.com>
2025-03-14 19:24:19 +0700

    Add Crabby enemy and implement basic AI and rendering
    Created Crabby class as an enemy type with animations and basic state handling.
    Implemented EnemyManager to manage multiple crabs and their behavior in the game.
    Added functionality for rendering and updating the crabby enemies with appropriate animations.
    Loaded enemy data from the level data to place the crabs at specific positions in the level.
    Integrated enemy rendering in the gameplay scene alongside the player and background elements.

--- MyGameCore/src/main/java/utilz/LoadSave.java
+++ MyGameCore/src/main/java/utilz/LoadSave.java
@@ -1,5 +1,6 @@
 package utilz;
 
+import entities.Crabby;
 import game.Game;
 
 import javax.imageio.ImageIO;
@@ -7,13 +8,16 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+
+import static utilz.Constants.EnemyConstants.CRABBY;
 
 public class LoadSave {
 
     public static final String PLAYER_ATLAS = "player_sprites.png";
     public static final String LEVEL_ATLAS = "outside_sprites.png";
-   // public static  final String LEVEL_ONE_DATA = "level_one_data.png";
-    public static final String LEVEL_ONE_DATA = "level_one_data_long.png";
+    // public static  final String LEVEL_ONE_DATA = "level_one_data.png";
+    public static final String LEVEL_ONE_DATA = "level_one_data_longs.png";
     public static final String MENU_BUTTONS = "button_atlas.png";
     public static final String MENU_BACKGROUND = "menu_background.png";
     public static final String PAUSE_BACKGROUND = "pause_menu.png";
@@ -24,6 +28,7 @@ public class LoadSave {
     public static final String PLAYING_BG_IMG = "playing_bg_img.png";
     public static final String BIG_CLOUD = "big_clouds.png";
     public static final String SMALL_CLOUD = "small_clouds.png";
+    public static final String CRABBY_SPRITE = "crabby_sprite.png";
 
     public static BufferedImage GetSpriteAtlas(String fileName) {
 
@@ -43,6 +48,20 @@ public class LoadSave {
             }
         }
         return img;
+    }
+
+    public static ArrayList<Crabby> GetCrabs(){
+        BufferedImage img = GetSpriteAtlas(LEVEL_ONE_DATA);
+        ArrayList<Crabby> list = new ArrayList<>();
+
+        for(int j = 0; j < img.getHeight(); j++)
+            for (int i = 0; i < img.getWidth(); i++) {
+                Color color = new Color(img.getRGB(i, j));
+                int value = color.getGreen();
+                if(value == CRABBY)
+                    list.add(new Crabby(i * Game.TILES_SIZE, j * Game.TILES_SIZE));
+            }
+        return list;
     }
 
     public static int[][] GetLevelData() {
