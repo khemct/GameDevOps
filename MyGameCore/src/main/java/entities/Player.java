b6881b5c124afff214b02668ed84964a27ebb5d5
KhemCT <chadchapong11043@gmail.com>
2025-03-15 14:27:26 +0700

    Add health bar system and HP display to Player entity
    Added a health bar system to the Player entity.
    - The health bar is displayed on the screen and reflects the player's current health.
    - A health bar image (statusBarImg) is loaded and rendered on the UI.
    - The player's health changes using the `changeHealth(int value)` method, which adjusts the health value and updates the health bar width accordingly.
    - The health bar is updated every frame using the `updateHealthBar()` method.

--- MyGameCore/src/main/java/entities/Player.java
+++ MyGameCore/src/main/java/entities/Player.java
@@ -25,6 +25,23 @@ public class Player extends Entity {
     private float fallSpeedAfterCollision = 0.5f * Game.SCALE;
     private boolean inAir = false;
 
+    //Status Bar
+    private BufferedImage statusBarImg;
+
+    private int statusBarWidth = (int) (192 *  Game.SCALE);
+    private int statusBarHeight = (int) (58 *  Game.SCALE);
+    private int statusBarX = (int) (10 *  Game.SCALE);
+    private int statusBarY = (int) (10 *  Game.SCALE);
+
+    private int healthBarWidth = (int) (150 *  Game.SCALE);
+    private int healthBarHeight = (int) (4 *  Game.SCALE);
+    private int healthBarXStart = (int) (34 *  Game.SCALE);
+    private int healthBarYStart = (int) (14 *  Game.SCALE);
+
+    private int maxHealth = 100;
+    private int currentHealth = maxHealth;
+    private int healthWidth = healthBarWidth;
+
     public Player(float x, float y, int width, int height) {
         super(x, y, width, height);
         loadAnimations();
@@ -33,14 +50,28 @@ public class Player extends Entity {
     }
 
     public void update() {
+        updateHealthBar();
+
         updatePos();
         updateAnimationTick();
         setAnimation();
     }
 
+    private void updateHealthBar() {
+        healthWidth = (int) ((currentHealth / (float)maxHealth) * healthBarWidth) ;
+    }
+
     public void render(Graphics g, int lvlOffset) {
         g.drawImage(animations[playerAction][aniIndex], (int) (hitbox.x - xDrawOffset) - lvlOffset, (int) (hitbox.y - yDrawOffset), width, height, null);
 //		drawHitbox(g, lvlOffset);
+
+        drawUI(g);
+    }
+
+    private void drawUI(Graphics g) {
+        g.drawImage(statusBarImg,statusBarX,statusBarY,statusBarWidth,statusBarHeight,null);
+        g.setColor(Color.red);
+        g.fillRect(healthBarXStart + statusBarX,healthBarYStart + statusBarY,healthWidth,healthBarHeight);
     }
 
     private void updateAnimationTick() {
@@ -147,6 +178,16 @@ public class Player extends Entity {
 
     }
 
+    public void changeHealth(int value){
+        currentHealth += value;
+
+        if(currentHealth <= 0) {
+            currentHealth = 0;
+            //gameOver();
+        }else if( currentHealth >= maxHealth)
+            currentHealth = maxHealth;
+    }
+
     private void loadAnimations() {
 
         BufferedImage img = LoadSave.GetSpriteAtlas(LoadSave.PLAYER_ATLAS);
@@ -155,6 +196,8 @@ public class Player extends Entity {
         for (int j = 0; j < animations.length; j++)
             for (int i = 0; i < animations[j].length; i++)
                 animations[j][i] = img.getSubimage(i * 64, j * 40, 64, 40);
+
+        statusBarImg = LoadSave.GetSpriteAtlas(LoadSave.STATUS_BAR);
 
     }
 
