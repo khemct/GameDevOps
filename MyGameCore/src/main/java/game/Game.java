cf5c0a742a2dcc03a48c3e599b01e6dcf05002fa
KhemCT <chadchapong11043@gmail.com>
2025-03-17 19:05:15 +0700

    Add audio options with volume control and mute buttons to pause menu
    - Added an `AudioOptions` class to manage the sound and volume control.
    - Integrated volume slider and mute buttons for music and sound effects (SFX) in the pause menu.
    - Updated the `PauseOverlay` class to include audio options for adjusting the volume and toggling sound on/off.
    - Created `SoundButton` and `VolumeButton` classes for handling individual sound and volume controls.
    - Implemented functionality to adjust the volume and toggle sound on and off through the buttons.
    - Integrated these changes into the `Game` class, allowing access to the audio options from the pause menu.

--- MyGameCore/src/main/java/game/Game.java
+++ MyGameCore/src/main/java/game/Game.java
@@ -4,6 +4,7 @@ import entities.Player;
 import gamestates.Gamestate;
 import gamestates.Playing;
 import gamestates.Menu;
+import ui.AudioOptions;
 import utilz.LoadSave;
 
 import java.awt.*;
@@ -18,6 +19,7 @@ public class Game implements Runnable {
 
     private Playing playing;
     private Menu menu;
+    private AudioOptions audioOptions;
 
     public final static int TILES_DEFAULT_SIZE = 32;
     public final static float SCALE = 2f;
@@ -41,6 +43,7 @@ public class Game implements Runnable {
     }
 
     private void initClasses() {
+        audioOptions = new AudioOptions();
         menu = new Menu(this);
         playing = new Playing(this);
     }
@@ -136,5 +139,9 @@ public class Game implements Runnable {
 
     public Playing getPlaying() {
         return playing;
+    }
+
+    public AudioOptions getAudioOptions(){
+        return audioOptions;
     }
 }
